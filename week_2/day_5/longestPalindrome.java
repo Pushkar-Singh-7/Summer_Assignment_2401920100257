73d8ba798064a3b5e09a5b82b06152a7dfe07b9b
pushkar singh <pushkarrsingh777@gmail.com>
2026-06-12 22:21:21 +0530

    Create longestPalindrome.java

---
+++ week_2/day_5/longestPalindrome.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        int n = s.length();
+        int start = 0, maxLen = 1;
+        for (int i = 0; i < n; i++) {
+            int l = i, r = i;
+            while (l >= 0 && r < n && s.charAt(l) == s.charAt(r)) {
+                if (r - l + 1 > maxLen) {
+                    maxLen = r - l + 1;
+                    start = l;
+                }
+                l--;
+                r++;
+            }
+            l = i;
+            r = i + 1;
+            while (l >= 0 && r < n && s.charAt(l) == s.charAt(r)) {
+                if (r - l + 1 > maxLen) {
+                    maxLen = r - l + 1;
+                    start = l;
+                }
+                l--;
+                r++;
+            }
+        }
+        return s.substring(start, start + maxLen);
+    }
+}
