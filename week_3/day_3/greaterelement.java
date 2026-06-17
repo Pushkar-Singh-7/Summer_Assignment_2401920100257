74bf2a772e3052c4690cee9de6450ff258fd1be4
pushkar singh <pushkarrsingh777@gmail.com>
2026-06-18 01:04:40 +0530

    Create greaterelement.java

---
+++ week_3/day_3/greaterelement.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> st = new Stack<>();
+        for (int num : nums2) {
+            while (!st.isEmpty() && num > st.peek()) {
+                map.put(st.pop(), num);
+            }
+            st.push(num);
+        }
+        int[] result = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            result[i] = map.getOrDefault(nums1[i],-1);
+        }
+        return result;
+    }
+}
